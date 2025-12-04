e479e0aab9b311752ece96b11d1f79a3f0dbe130
Rohit2034 <Rohit.Kumar5@ust.com>
2025-12-04 17:43:14 +0530

    created access token , refresh token , verify password

---
+++ utils/security.py
@@ -0,0 +1,33 @@
+from datetime import datetime, timedelta,timezone
+from jose import JWTError, jwt
+from fastapi import Depends, HTTPException
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from database import collections
+from passlib.context import CryptContext
+from dotenv import load_dotenv
+import os
+load_dotenv()
+# === CONFIG ===
+SECRET_KEY =os.getenv("SECRET_KEY")  
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+REFRESH_TOKEN_EXPIRE_DAYS = int(os.getenv("REFRESH_TOKEN_EXPIRE_DAYS"))
+
+pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
+bearer_scheme = HTTPBearer()
+
+# === PASSWORD & JWT ===
+def verify_password(plain: str, hashed: str) -> bool:
+    return pwd_context.verify(plain, hashed)
+
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire, "type": "access"})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+def create_refresh_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire =datetime.now(timezone.utc) + timedelta(days=REFRESH_TOKEN_EXPIRE_DAYS)
+    to_encode.update({"exp": expire, "type": "refresh"})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
