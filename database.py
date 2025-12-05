5854c29b4c1595d4d7c593c2e1119624caf63c30
ArjunJS08 <303391@ust.com>
2025-12-05 12:12:39 +0530

    db config updated and gitignore added

---
+++ database.py
@@ -0,0 +1,19 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+client = AsyncIOMotorClient(os.getenv("MONGODB_CLIENT"))
+db = client.talent_management
+
+collections = {
+    "employees": db.employees,
+    "applications": db.applications,
+    "users": db.users,
+    "refresh_tokens": db.refresh_tokens,
+    "audit_logs": db.audit_logs,
+    "block_list_tokens":db.block_list_tokens,
+    "resource_request":db.resource_request
+}
