f631ef1fc6fa6e0815777f0c3314e7c4adf7c492
Sovan-Mohanty <303366@ust.com>
2025-12-05 12:14:46 +0530

    added the dependency injection

--- utils/security.py
+++ utils/security.py
@@ -31,3 +31,23 @@ def create_refresh_token(data: dict) -> str:
     expire =datetime.now(timezone.utc) + timedelta(days=REFRESH_TOKEN_EXPIRE_DAYS)
     to_encode.update({"exp": expire, "type": "refresh"})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+# === AUTH DEPENDENCY ===
+async def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(bearer_scheme)):
+    token = credentials.credentials
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        emp_id: str = payload.get("sub")
+        token_type: str = payload.get("type")
+        role: str = payload.get("role")
+
+        if not emp_id or token_type != "access":
+            raise HTTPException(status_code=401, detail="Invalid token")
+
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid or expired token")
+
+    user = await collections["users"].find_one({"employee_id": emp_id})
+    if not user:
+        raise HTTPException(status_code=401, detail="User not found")
+
+    return {"employee_id": emp_id, "role": user["role"], "user": user}
