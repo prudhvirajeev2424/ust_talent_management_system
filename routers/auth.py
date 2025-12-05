6fcef74d59fc54d0f40f4aab6395a9746410de20
sohan-kumar-rout20 <303373@ust.com>
2025-12-05 11:53:16 +0530

    Created refresh tokenapi

--- routers/auth.py
+++ routers/auth.py
@@ -37,3 +37,48 @@ async def login(username: str, password: str):
         "token_type": "bearer",
         "expires_in": "300 seconds"
     }
+    
+@router.post("/refresh")
+async def refresh_token(refresh_token: str):
+    try:
+        payload = jwt.decode(refresh_token, SECRET_KEY, algorithms=[ALGORITHM])
+        emp_id = payload.get("sub")
+        token_type = payload.get("type")
+
+        if token_type != "refresh":
+            raise HTTPException(status_code=401, detail="Invalid refresh token")
+
+        stored = await collections["refresh_tokens"].find_one({"token": refresh_token})
+        if not stored:
+            raise HTTPException(status_code=401, detail="Refresh token revoked")
+
+    except ExpiredSignatureError:
+        raise HTTPException(status_code=401, detail="Refresh token expired")
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid refresh token")
+
+    user = await collections["users"].find_one({"employee_id": emp_id})
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    new_access_token = create_access_token({"sub": emp_id, "role": user["role"]})
+    new_refresh_token = create_refresh_token({"sub": emp_id, "role": user["role"]})
+
+    last_token = await collections["refresh_tokens"].find_one(
+        {"employee_id": emp_id}, sort=[("created_at", -1)]
+    )
+    if last_token:
+        await collections["refresh_tokens"].delete_one({"_id": last_token["_id"]})
+
+    await collections["refresh_tokens"].insert_one({
+        "token": new_refresh_token,
+        "employee_id": emp_id,
+        "created_at":datetime.now(timezone.utc),
+        "expires_at":datetime.now(timezone.utc) + timedelta(days=7)
+    })
+
+    return {
+        "access_token": new_access_token,
+        "token_type": "bearer",
+        "expires_in": "300 seconds"
+    }
